79ae928cd504d933bad6602c962b90948a9b0d0c
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-09-09 01:16:11 +0300

    Remove redundant comment

--- src/libs/utils/date.ts
+++ src/libs/utils/date.ts
@@ -1,4 +1,3 @@
-// A function that transforms utc date string to localised string
 export function getLocalisedDate(date: string) {
   const utcDate = new Date(date)
   const localDate = utcDate.toLocaleString()
