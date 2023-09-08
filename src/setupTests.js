bed18e02cc70871d4acbc65df3f022341f77e14e
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-09-08 21:22:19 +0300

    Mock window

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@ import '@testing-library/jest-dom'
 HTMLElement.prototype.scrollBy = () => {}
 HTMLElement.prototype.scrollIntoView = () => {}
 HTMLElement.prototype.scrollTo = () => {}
+
+jest.spyOn(window, 'scrollTo').mockImplementation(() => {})
+jest.spyOn(window, 'alert').mockImplementation(() => {})
