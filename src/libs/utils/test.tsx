6886f3dc1095acb29d35fd702021414d9bb7442d
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-09-09 23:43:03 +0300

    Remove redundant code

--- src/libs/utils/test.tsx
+++ src/libs/utils/test.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from 'react'
-import { MemoryRouter, MemoryRouterProps, Router } from 'react-router-dom'
+import { MemoryRouter, MemoryRouterProps } from 'react-router-dom'
 import { ChatContext } from 'src/containers/ChatProvider/ChatContext'
 import { ChatClient, ConnectParams } from '../chat-room/client'
-
-import { createBrowserHistory } from 'history'
 
 export const withMockedConsoleError =
   <Func extends Function>(fn: Func) =>
@@ -23,44 +21,8 @@ export const withRouter = (children: ReactNode, options?: WithRouterOptions) => 
   <MemoryRouter {...options}>{children}</MemoryRouter>
 )
 
-// export type withRouterOptions = {
-//   basename?: string
-//   history?: Partial<History>
-//   state?: Record<string, any>
-// }
-
-// export const withRouter = (children: ReactNode, options?: withRouterOptions) => {
-//   const basename = options?.basename || '/'
-//   let history = createBrowserHistory()
-//   let state
-
-//   if (options?.basename) {
-//     state = options.state
-//   }
-
-//   if (options?.history) {
-//     history = { ...history, ...options.history }
-//   }
-
-//   return (
-//     <Router basename={basename} navigator={history} location={{ ...history.location, state }}>
-//       {children}
-//     </Router>
-//   )
-// }
-
 export class ChatClientMock extends ChatClient {
-  connect = jest.fn(
-    ({
-      userName,
-      roomName,
-      onNewMessage,
-      onClose,
-      onError,
-      onGetMessageHistory,
-      onGetUsers,
-    }: ConnectParams) => {},
-  )
+  connect = jest.fn((params: ConnectParams) => {})
   disconnect = jest.fn(() => Promise.resolve())
   sendMessage = jest.fn(() => Promise.resolve())
 }
