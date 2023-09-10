5a6abad349d19d40a323b9d98090f696567062e1
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2023-09-10 14:15:58 +0300

    Add comment

--- src/containers/ChatProvider/ChatProvider.tsx
+++ src/containers/ChatProvider/ChatProvider.tsx
@@ -5,6 +5,7 @@ import { ChatClient } from 'src/libs/chat-room/client'
 import { ChatContext } from './ChatContext'
 
 export default function ChatProvider({ children }: PropsWithChildren<{}>) {
+  // Providing `ChatClient` through context makes it easier to mock the `ChatClient` in component tests.
   const [chatClient] = useState(() => new ChatClient())
 
   return <ChatContext.Provider value={{ chatClient }}>{children}</ChatContext.Provider>
